2ac833af010698f32ffee3ac305d9bec8861000a
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-07 19:45:02 +0700

    equalize the array

--- CplusplusSample/SampleProject/HackerRank/equalizeTheArray.cpp
+++ CplusplusSample/SampleProject/HackerRank/equalizeTheArray.cpp
@@ -1,30 +1,30 @@
-/*
-sort the array
-max = 0
-count = 1
-for (i != arr.size && i != arr.size - 1){
-
-    if arr[i] == arr[i + 1] -> count + 1
-                               if count > max -> max = count
-    else -> count = 1
-}
-return arr.size - max
-*/
-int equalizeArray(vector<int> arr) {
-    sort(arr.begin(), arr.end());
-    int max = 1;
-    int count = 1;
-    for (int i = 0; i != arr.size() - 1; i++) {
-        if (arr[i] == arr[i + 1]) {
-            count++;
-            if (count > max) {
-                max = count;
-            }
-        }
-        else {
-            count = 1;
-        }
-    }
-    cout << "max = " << max;
-    return arr.size() - max;
-}
+///*
+//sort the array
+//max = 0
+//count = 1
+//for (i != arr.size && i != arr.size - 1){
+//
+//    if arr[i] == arr[i + 1] -> count + 1
+//                               if count > max -> max = count
+//    else -> count = 1
+//}
+//return arr.size - max
+//*/
+//int equalizeArray(vector<int> arr) {
+//    sort(arr.begin(), arr.end());
+//    int max = 1;
+//    int count = 1;
+//    for (int i = 0; i != arr.size() - 1; i++) {
+//        if (arr[i] == arr[i + 1]) {
+//            count++;
+//            if (count > max) {
+//                max = count;
+//            }
+//        }
+//        else {
+//            count = 1;
+//        }
+//    }
+//    cout << "max = " << max;
+//    return arr.size() - max;
+//}
