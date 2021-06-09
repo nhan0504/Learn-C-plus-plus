411bd6ff5ca20b1ca3791dd5d23a17b88bf0b55d
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-09 23:01:29 +0700

    Create a table with n rows and n columns

---
+++ CplusplusSample/Table/Table.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//int main() {
+//	int number;
+//	cin >> number;
+//	vector<int> line(number,0);
+//	int input = 1;
+//	vector<vector<int>> table(number,line);
+//	for (int i = 0; i < number; i++) {
+//		for (int j = 0; j < number; j++) {
+//			table[i][j] = input;
+//			cout << "|" << table[i][j];	
+//			input++;
+//		}
+//		cout <<"|" << endl;
+//	}
+//	return 0;
+//}
