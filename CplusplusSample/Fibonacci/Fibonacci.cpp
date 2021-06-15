be8d1ec74bdb146c9641a6460f6f0d1d9c72b718
Nhan0504 <ttn0504ltt@gmail.com>
2021-06-15 11:48:39 +0700

    Shoter version

--- CplusplusSample/Fibonacci/Fibonacci.cpp
+++ CplusplusSample/Fibonacci/Fibonacci.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
 #include<vector>
-#include "Fibonacci.h"
 using namespace std;
 
-int fibonacci_vector(int n, vector<int> fibonacci) {
+int fibonacci_vector(int n) {
+	vector<int> fibonacci(2,1);
 	int i = 0;
 	int a;
 	while (i != n - 2) {
-		cout << "i = " << i << endl;
 		a = fibonacci[i] + fibonacci[i + 1];
-		cout << "a = " << a << endl;
 		fibonacci.push_back(a);
 		i++;
 	}
-	for (int j = 0; j != fibonacci.size(); j++) {
-		cout << fibonacci[j] << "  ";
-	}
-	cout << endl;
-	return fibonacci[(n - 1)];
+	return a;
 }
 
 int main()
 {
-	vector<int> fibonacci;
-	fibonacci.push_back(1);
-	fibonacci.push_back(1);
 	int n;
-	
 	while (true) {
 		cout << "Input: ";
 		cin >> n;
-		int a = fibonacci_vector(n, fibonacci);
-		cout << a << endl;
+		cout << fibonacci_vector(n) << endl;
 	}
 	return 0;
 }
